a07048c2d5c253c415525cb7aa2dc95e2712f60e
dymafr <erwan1309@hotmail.com>
2022-03-25 17:12:22 +0100

    1 file modified, 1 added

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,9 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue';
+import { useIdleTime } from './useIdleTime';
 
-const secondes = ref(0);
-const totalIdleTime = ref(0);
-
-setInterval(() => {
-  secondes.value++;
-  totalIdleTime.value += secondes.value;
-}, 1000);
-
-function mouseMove(event: MouseEvent) {
-  secondes.value = 0;
-}
-
-onMounted(() => window.addEventListener('mousemove', mouseMove));
-onUnmounted(() => window.removeEventListener('mousemove', mouseMove));
+const { secondes, totalIdleTime } = useIdleTime();
 </script>
 
 <style lang="scss">
