89b0e7ac68e6bde92d8521d32374c1e477c2910e
dymafr <erwan1309@hotmail.com>
2022-03-25 17:07:48 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
-<template></template>
+<template>
+  <h1>{{ secondes }}</h1>
+</template>
 
 <script setup lang="ts">
-import { defineAsyncComponent, ref } from 'vue';
+import { ref, onMounted, onUnMounted } from 'vue';
+
+const secondes = ref(0);
+
+setInterval(() => {
+  secondes.value++;
+}, 1000);
+
+function mouseMove(event: MouseEvent) {
+  secondes.value = 0;
+}
+
+onMounted(() => window.addEventListener('mousemove', mouseMove));
+onUnMounted(() => window.removeEventListener('mousemove', mouseMove));
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import './assets/scss/base.scss';
+</style>
